d4345e8dfc588dd1e83826f520c347402a4755c9
Матвей Радченко <MVRadchenko@alfabank.ru>
2025-02-25 23:07:47 +0100

    fix: понижение версий пакетов для совместимости с Node.js 16
    - @reduxjs/toolkit понижен с 2.0.1 до 1.9.7
    - react-redux понижен с 9.0.4 до 8.1.3
    - stylelint понижен с 16.14.1 до 15.11.0
    - stylelint-config-standard понижен с 37.0.0 до 34.0.0

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import App from './App'
+import 'index.css'
 import { themeConfig } from './theme/themeConfig'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
