3a10baacd372df261ccd1cacc6939d0ffad5a681
Dmitrii Starkov <starkoffbusiness@gmail.com>
2025-03-04 00:01:30 +0500

    Added basic reset style

--- packages/client/src/app/app.tsx
+++ packages/client/src/app/app.tsx
@@ -2,6 +2,8 @@ import { themeConfig } from './theme-config'
 import { ConfigProvider } from 'antd'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import './app.css'
+import '../shared/config/variables.pcss'
 
 export function App() {
   return (
