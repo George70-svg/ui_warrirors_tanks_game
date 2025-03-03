65fbe0bac62e7bb6dade3da8e61a0fadc4e22254
Dmitrii Starkov <starkoffbusiness@gmail.com>
2025-03-04 00:02:59 +0500

    Added basic tokens for antdesign themeConfig

--- packages/client/src/app/theme-config.ts
+++ packages/client/src/app/theme-config.ts
@@ -1,3 +1,10 @@
 import type { ThemeConfig } from 'antd'
 
-export const themeConfig: ThemeConfig = {}
+export const themeConfig: ThemeConfig = {
+  token: {
+    fontFamily: 'Nunito Sans, sans-serif',
+    colorText: '#ffffff',
+    colorIcon: '#ffffff',
+    colorTextPlaceholder: '#ffffff',
+  },
+}
