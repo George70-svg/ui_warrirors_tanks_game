48408b56b55cf6ed251a3bcf9d53ed14452a4c48
Dmitry Khokhlov <d.hohlov@corp.mail.ru>
2025-03-18 23:17:24 +0300

    Mpve files back

--- packages/client/src/app/main.tsx
+++ packages/client/src/app/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './ui/app'
-import { App as AntApp, ConfigProvider } from 'antd'
-import { themeConfig } from './theme-config'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AntApp>
-      <ConfigProvider theme={themeConfig}>
-        <App />
-      </ConfigProvider>
-    </AntApp>
+    <App />
   </React.StrictMode>
 )
--- packages/client/src/app/ui/app.tsx
+++ packages/client/src/app/ui/app.tsx
@@ -3,11 +3,18 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routing/router'
 import './app.pcss'
 import { Provider } from 'react-redux'
+import { App as AntApp, ConfigProvider } from 'antd'
+import { themeConfig } from './theme-config'
+import React from 'react'
 
 export function App() {
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <AntApp>
+      <ConfigProvider theme={themeConfig}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </ConfigProvider>
+    </AntApp>
   )
 }
