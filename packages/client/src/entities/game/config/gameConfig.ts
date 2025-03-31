6552887a48c8fb09616a978ef4b364f7a1fd78da
Anton <noname@example.com>
2025-03-31 21:19:20 +0500

    Add game map

--- packages/client/src/entities/game/config/gameConfig.ts
+++ packages/client/src/entities/game/config/gameConfig.ts
@@ -1,17 +1,9 @@
 import { Tank } from '../objects/tank'
-import { Decoration, DecorationProps } from '../objects/decoration'
+import { Decoration } from '../objects/decoration'
 import { Bullet } from '../objects/bullet'
 import tankImg from '../../../assets/images/tank.png'
-import brickImg from '../../../assets/images/brick.png'
-import metalImg from '../../../assets/images/metal.png'
 import { ComputerTank } from '../objects/computerTank'
-import {
-  FIGURE1_COORDS,
-  FIGURE2_COORDS,
-  FIGURE3_COORDS,
-  FIGURE4_COORDS,
-} from '../constants/decorations'
-import { Coordinate } from '../types'
+import { getDecorations } from '../constants/decorations'
 
 export const CELL_SIZE = 50
 
@@ -37,35 +29,12 @@ export const config: Config = {
   score: 0,
 }
 
-function createDecorationFigure(
-  coords: Coordinate[],
-  context: CanvasRenderingContext2D,
-  type: NonNullable<DecorationProps['typeDecoration']>,
-  hasDeletable?: DecorationProps['markForDelete'],
-  imageSrc?: string,
-  color?: string
-): Decoration[] {
-  return coords.map(
-    ({ x: coordX, y: coordY }) =>
-      new Decoration({
-        id: crypto.randomUUID(),
-        context,
-        position: { x: toPixels(coordX), y: toPixels(coordY) },
-        size: { width: toPixels(1), height: toPixels(1) },
-        color,
-        typeDecoration: type,
-        imageSrc,
-        hasDeletable,
-      })
-  )
-}
-
 export function initializeTankObjects(context: CanvasRenderingContext2D) {
   config.tankObjects = [
     new Tank({
       id: crypto.randomUUID(),
       context,
-      startPosition: { x: toPixels(13), y: toPixels(7) },
+      startPosition: { x: toPixels(13), y: toPixels(12) },
       direction: 'up',
       speed: 0.15,
       size: { width: 50, height: 64 },
@@ -77,72 +46,5 @@ export function initializeTankObjects(context: CanvasRenderingContext2D) {
 }
 
 export function initializeDecorationObjects(context: CanvasRenderingContext2D) {
-  const hasDeletable = true
-  const figure1 = createDecorationFigure(
-    FIGURE1_COORDS,
-    context,
-    'brick',
-    hasDeletable,
-    brickImg
-  )
-  const figure2 = createDecorationFigure(
-    FIGURE2_COORDS,
-    context,
-    'brick',
-    hasDeletable,
-    brickImg
-  )
-  const figure3 = createDecorationFigure(
-    FIGURE3_COORDS,
-    context,
-    'metal',
-    !hasDeletable,
-    metalImg
-  )
-  const figure4 = createDecorationFigure(
-    FIGURE4_COORDS,
-    context,
-    'metal',
-    !hasDeletable,
-    metalImg
-  )
-
-  config.decorationObjects = [
-    ...figure1,
-    ...figure2,
-    ...figure3,
-    ...figure4,
-    new Decoration({
-      id: crypto.randomUUID(),
-      context,
-      position: { x: toPixels(3), y: toPixels(3) },
-      size: { width: toPixels(1), height: toPixels(1) },
-      color: '#46efe9',
-      hasDeletable: false,
-    }),
-    new Decoration({
-      id: crypto.randomUUID(),
-      context,
-      position: { x: toPixels(10), y: toPixels(5) },
-      size: { width: toPixels(1), height: toPixels(1) },
-      color: '#0BA5EC',
-      hasDeletable: false,
-    }),
-    new Decoration({
-      id: crypto.randomUUID(),
-      context,
-      position: { x: toPixels(14), y: toPixels(8) },
-      size: { width: toPixels(1), height: toPixels(1) },
-      color: '#46efe9',
-      hasDeletable: false,
-    }),
-    new Decoration({
-      id: crypto.randomUUID(),
-      context,
-      position: { x: toPixels(20), y: toPixels(10) },
-      size: { width: toPixels(1), height: toPixels(1) },
-      color: '#0BA5EC',
-      hasDeletable: false,
-    }),
-  ]
+  config.decorationObjects = getDecorations(context)
 }
