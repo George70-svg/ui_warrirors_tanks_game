27df3ff83342071330a5a22f862c5112e127aa0c
Nikita R <nrrozhkov@gmail.com>
2025-04-01 16:15:18 +0300

    Очищение кэша

--- packages/client/public/sw.js
+++ packages/client/public/sw.js
@@ -9,37 +9,44 @@ const URLS = [
   '/images/tank.png',
 ]
 
-this.addEventListener('install', (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(URLS)
-      })
-      .catch((err) => {
-        console.error(err)
-        throw err
-      })
+      .then((cache) => cache.addAll(URLS))
+      .catch((err) => console.error('Cache open failed: ', err))
   )
 })
 
-this.addEventListener('activate', function (event) {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(cacheNames.map((name) => caches.delete(name)))
-    })
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((name) => {
+            if (name !== CACHE_NAME) {
+              console.log(`Deleting old cache: ${name}`)
+              return caches.delete(name)
+            }
+          })
+        )
+      })
+      .then(() => {
+        console.log('Claiming clients...')
+        return self.clients.claim()
+      })
   )
 })
 
-this.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
         return response
       }
 
-      const fetchRequest = event.request.clone()
-      return fetch(fetchRequest).then((response) => {
+      return fetch(event.request).then((response) => {
         if (!response || response.status !== 200 || response.type !== 'basic') {
           return response
         }
@@ -48,6 +55,7 @@ this.addEventListener('fetch', (event) => {
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, responseToCache)
         })
+
         return response
       })
     })
