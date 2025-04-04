6ba0ec565f76acfe02840815ca90d43f31009ccf
Dmitry Khokhlov <d.hohlov@corp.mail.ru>
2025-04-05 00:30:31 +0300

    add production SSR build

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,18 +3,40 @@ import cors from 'cors'
 dotenv.config()
 
 import express from 'express'
-import { createClientAndConnect } from './db'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import * as console from 'node:console'
 
-const app = express()
-app.use(cors())
-const port = Number(process.env.SERVER_PORT) || 3001
+async function startServer() {
+  const app = express()
+  app.use(cors())
+  const port = Number(process.env.SERVER_PORT) || 3001
 
-createClientAndConnect()
+  const distPath = path.dirname(require.resolve('client/dist/index.html'))
+  const ssrClientPath = require.resolve('client/dist-ssr/client.cjs')
 
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
-})
+  app.use('/assets', express.static(path.resolve(distPath, 'assets')))
 
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-})
+  app.use('*', async (_, res, next) => {
+    try {
+      const template = fs.readFileSync(
+        path.resolve(distPath, 'index.html'),
+        'utf-8'
+      )
+
+      const { render } = await import(ssrClientPath)
+      const appHtml = render(template)
+
+      const html = template.replace('<!--ssr-outlet-->', appHtml)
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+    } catch (e) {
+      next(e)
+    }
+  })
+
+  app.listen(port, () => {
+    console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+  })
+}
+
+startServer()
