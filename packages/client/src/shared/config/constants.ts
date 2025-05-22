cd27353be9a992a6ea33e3df76160c6abde77cde
nrrozhkov <77511976+nrrozhkov@users.noreply.github.com>
2025-05-22 18:44:28 +0300

    Update constants.ts

--- packages/client/src/shared/config/constants.ts
+++ packages/client/src/shared/config/constants.ts
@@ -1,3 +1,3 @@
 export const RESOURCE_URL = 'https://ya-praktikum.tech/api/v2/resources'
 // @ts-ignore
-export const OAUTH_YA_REDIRECT_URI = `http://${__HOST__}:${__PORT__}`
+export const OAUTH_YA_REDIRECT_URI = 'http://84.252.138.94:3000/'
